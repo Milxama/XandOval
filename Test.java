d8924cad9096240c80b7cc98fc7a4aa784c42756
Milxama <38768164+Milxama@users.noreply.github.com>
2018-05-25 19:31:47 +0300

    The game(version 1)

---
+++ Test.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Test{
+	public static void main(String[] args){
+	    XandOval game=new XandOval();
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    game.setSize(1200,1200);
+		game.setVisible(true);
+		
+	}
+	
+	
+}
