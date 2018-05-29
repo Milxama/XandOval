20bdbae8394cdf375a7bc3bfb31c403bbd570ab5
Milxama <38768164+Milxama@users.noreply.github.com>
2018-05-29 21:08:16 +0300

    X and Oval (Version 3)
    Game

--- Test.java
+++ Test.java
@@ -4,7 +4,8 @@ public class Test{
 	public static void main(String[] args){
 	    XandOval game=new XandOval();
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    game.setSize(1200,1200);
+		game.setLocation(500,150);
+	    game.setSize(750,800);
 		game.setVisible(true);
 		
 	}
